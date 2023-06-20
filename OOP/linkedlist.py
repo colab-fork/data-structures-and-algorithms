fbae7c50d9975c275ee5294ef04a833691173864
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2023-06-20 13:52:56 +0530

    Create linkedlist.py

---
+++ OOP/linkedlist.py
@@ -0,0 +1,44 @@
+class Node:
+    
+    def __init__(self, data): #every single node has two attributes -> data, next
+        self.data = data
+        self.next = None
+        
+    def  __repr__(self):
+        return self.data
+
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None #only information you need is where the L list starts
+    
+    def __repr__(self): 
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return "->".join(nodes)
+
+
+
+"""
+>>> %Run -c $EDITOR_CONTENT
+>>> llist = LinkedList()
+>>> llist
+None
+>>> first_node = Node('a')
+>>> llist.head = first_node
+>>> llist
+a->None
+>>> second_node = Node("b")
+>>> third_node = Node("c")
+>>> first_node.next = second_node
+>>> second_node.next = third_node
+>>> llist
+a->b->c->None
+>>> 
+
+"""
