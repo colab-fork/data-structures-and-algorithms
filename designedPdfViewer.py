5e5b8d9536c7924b22a84f6258eb28b71e434313
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2023-06-24 15:29:29 +0530

    Create designedPdfViewer.py

---
+++ designedPdfViewer.py
@@ -0,0 +1,22 @@
+h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+word = 'abc'
+def designerPdfViewer(h, word):
+    import string
+    idx =[]
+    tall = []
+    ret = 1
+    for i in word:
+        for j in range(len(let)):
+            if let[j] == i:
+                idx.append(j)
+    for i in lis:
+        his.append(h[i])
+    print(max(tall) * len(word))
+designerPdfViewer(h, word)
+
+
+"""
+>>> %Run -c $EDITOR_CONTENT
+9
+
+"""
