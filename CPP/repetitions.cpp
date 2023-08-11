23d57853ea4562fcff597a2c3892a5f7b7ebaf18
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2023-08-12 00:57:30 +0530

    Create repetitions.cpp

---
+++ CPP/repetitions.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define ll long long
+int main() 
+{
+    string s;
+    cin >> s;
+    int ans = 0, c = 0;
+    char l = 'A';
+    for(char d : s)
+        if (d==l) {
+            ++c;
+            ans = max(c, ans);
+        }
+        else {
+            l = d;
+            c = 1; 
+        }
+    cout << ans; 
+    
+}
+
+/*
+ATTCGGGA
+3
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
+
+*/
