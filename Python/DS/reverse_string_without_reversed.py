ee16e8bc88b7a9055d269aaadeecbafbbd2af588
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2024-06-22 00:15:22 +0530

    Update reverse_string_without_reversed.py

--- Python/DS/reverse_string_without_reversed.py
+++ Python/DS/reverse_string_without_reversed.py
@@ -3,16 +3,30 @@
 def reverse_string(s):
     reversed=''
     for i in range(len(s)-1, -1, -1):
-        reversed += s[i]
-    print(reversed)
+        reversed_string += s[i]
+    print(reversed_string)
+
+# Alternate method using stacks.
+
+def revered_string_stack(s):
+    stack= []
+    for i in s:
+       stack.append(i)
+    reversed_string = '' 
+    while stack:
+        reversed_string += stack.pop()
+    print(reversed_string)
 
 
-reverse_string('Shahid')
+string = 'Shahid'
+
+reversed_string_stack(string)
+reversed_string_stack(string)
 
 
 """
 
 >>> python t.py
 dihahS
-
+dihahS
 """
